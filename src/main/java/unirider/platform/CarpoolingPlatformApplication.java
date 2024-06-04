aa89417acabe99d18675b6a085a542e389d985ba
Gustavo Pardo <u202120347@upc.edu.pe>
2024-06-04 09:48:59 -0500

    New proyect

---
+++ src/main/java/unirider/platform/CarpoolingPlatformApplication.java
@@ -0,0 +1,13 @@
+package unirider.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpoolingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarpoolingPlatformApplication.class, args);
+    }
+
+}
